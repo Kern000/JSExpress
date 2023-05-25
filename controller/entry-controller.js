ab70af8aec8264d9d03f602db8bc874aeab81c3e
unknown <holupkern@gmail.com>
2023-05-25 22:11:31 +0800

    updated methods

--- controller/entry-controller.js
+++ controller/entry-controller.js
@@ -1,7 +1,9 @@
 const {v4: uuidv4} = require("uuid")
 
 const httpStatus = require("http-status")
-const data = [];
+let data = [];
+
+// note that data has been changed from const to let to use filter method reassignment
 
 const create = (req, res) => {
 
@@ -32,9 +34,105 @@ const readID = (req, res) => {
 
 // To get the ID, http://localhost:3333/entry/63e59b55-f9f5-49f2-a47f-61f2c7570327 -> The router /:id is only a placeholder
 
+const updateOne = (req, res) => {
+
+    const dataToUpdate = req.body;
+
+    const foundByID = data.find((item) => item.id === req.params.id)
+
+// This finds the entire object not just the ID, hence you can use dataToUpdate which is req.body (An object you posted), you can update the value of the specific property you want to change.
+
+    if (foundByID){
+
+        for (const property in dataToUpdate){
+
+            foundByID[property] = dataToUpdate[property]
+            
+        }
+
+        return res.sendStatus(httpStatus.OK)
+
+    } else {
+        
+        return res.sendStatus(httpStatus.NOT_FOUND)
+    }
+}
+
+const deleteOne = (req, res) => {
+
+    const foundByID = data.find((item) => item.id === req.params.id)
+
+    if (foundByID){
+
+        const findIndex = data.findIndex((item) => item.id === req.params.id)
+
+        if (findIndex > 0) {
+            
+            data.splice(findIndex, 1)
+
+        return res.sendStatus(httpStatus.OK)
+
+    } else {
+        
+        return res.sendStatus(httpStatus.NOT_FOUND)
+    }
+  }
+}
+
+
+const deleteEntriesWithID = (req, res) => {
+
+const foundByID = data.find((item) => item.id === req.params.id)
+
+if (foundByID){
+
+    data = data.filter((object) => object.id !== req.params.id)
+
+    return res.sendStatus(httpStatus.OK)
+
+} else {
+    
+    return res.sendStatus(httpStatus.NOT_FOUND)
+}
+}
+
+// Note that data.filter will copy the filtered array, so need to reassign it back to data
+// method 3 is to use lodash _.remove() based on the item.id === req.params.id
+
+const deleteProperty = (req, res) => {
+
+    const dataToDeleteProperty = req.body;
+
+    const foundByID = data.find((item) => item.id === req.params.id)
+
+    if (foundByID){
+
+        for (const property in dataToDeleteProperty){
+            
+            if (foundByID.hasOwnProperty(property)){
+
+            delete foundByID[property]
+            }
+        }
+
+        // delete function deletes both the property key and value
+
+        return res.sendStatus(httpStatus.OK)
+
+    } else {
+        
+        return res.sendStatus(httpStatus.NOT_FOUND)
+    }
+}
+
+
 
 module.exports = {
     create,
     readAll,
-    readID
+    readID,
+    updateOne,
+    deleteOne,
+    deleteProperty,
+    deleteEntriesWithID
 }
--- routes/entry.js
+++ routes/entry.js
@@ -1,13 +1,20 @@
 const express = require("express")
 const router = express.Router()
 
-const {create, readAll, readID} = require("../controller/entry-controller")
+const {create, readAll, readID, updateOne, deleteOne, deleteProperty, deleteEntriesWithID} = require("../controller/entry-controller")
 
 router.get("/", readAll)
 
 router.post("/", create)
 
 router.get("/:id", readID)
+
+router.put("/:id", updateOne)
+
+router.patch("/:id", deleteProperty)
+
+router.delete("/:id", deleteEntriesWithID)
+
 
 module.exports = {
     router
